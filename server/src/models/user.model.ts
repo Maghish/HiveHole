3eadf1f4bf8553267f886769687f45389e4f6713
Maghish <mail4mithran@gmail.com>
2024-07-08 17:12:31 +0530

    Added new fields in user model: email and password

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -4,9 +4,11 @@ const userSchema = new mongoose.Schema(
   {
     displayName: { type: String, required: true, maxLength: 50 },
     username: { type: String, required: true, maxLength: 30 },
+    email: { type: String, required: true, maxLength: 30 },
+    password: { type: String, required: true, maxLength: 40 },
     bio: { type: String, required: true, maxLength: 200 },
     // Profile photo URL, not the actual file
-    profilePhoto: { type: String, required: true },
+    profilePhoto: { type: String, required: false, default: "" },
     followers: { type: Number, required: false, default: 0 },
     // The username of the following people
     following: [{ type: String }],
@@ -14,7 +16,7 @@ const userSchema = new mongoose.Schema(
       type: String, // Online || Offline || Do Not Disturb
       default: "Offline",
     },
-    status: { type: String, maxLength: 100 },
+    status: { type: String, maxLength: 100, required: false },
     lastLogin: { type: String, required: false, default: Date.now() },
   },
   {
