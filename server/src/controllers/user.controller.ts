c9c84dd9a60055f90bb970ee7f43e651f2e10921
Maghish <mail4mithran@gmail.com>
2024-07-10 19:41:21 +0530

    Removed few routes

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -25,40 +25,4 @@ async function getUser(req: Request, res: Response): Promise<Response> {
   }
 }
 
-/**
- * Creates a new user from the given arguments
- * @param {import("express").Request} req
- * @param {import("express").Response} res
- * @returns {Promise<import("express").Response>}
- */
-async function createUser(req: Request, res: Response): Promise<Response> {
-  try {
-    const { displayName, username, email, password } = req.body;
-    let user = await UserModel.findOne({ username: username });
-    if (user) {
-      return res.status(400).json({
-        message: "Username is already taken, please pick a different username!",
-      });
-    }
-
-    user = new UserModel({
-      displayName: displayName,
-      username: username,
-      email: email,
-      password: password,
-      bio: "What's up! I just started using HiveHole!",
-      following: [],
-    });
-    user = await user.save();
-
-    return res
-      .status(200)
-      .json({ message: "Successfully created new user", user: user });
-  } catch (error) {
-    return res
-      .status(400)
-      .json({ message: `Unexpected error occurred: ${error}` });
-  }
-}
-
-export { getUser, createUser };
+export { getUser };
--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { getUser, createUser } from "../controllers/user.controller";
+import { getUser } from "../controllers/user.controller";
 
 const router = Router();
 
 router.get("/getuser/:username", getUser);
-router.post("/createuser", createUser);
 router.post("/edituser");
-router.delete("/deleteuser");
 router.get("/getuserfollowings/:username");
 router.post("/followuser");
 router.post("/unfollowuser");
