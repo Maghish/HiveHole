4b46f0fbc47a85e86363cdfe99f21f7bdc3cff0b
Maghish <mail4mithran@gmail.com>
2024-07-12 19:59:57 +0530

    Configured 'protect' middleware for every route except gethive route

--- server/src/routes/hive.route.ts
+++ server/src/routes/hive.route.ts
@@ -9,14 +9,15 @@ import {
   addHiveMember,
   removeHiveMember,
 } from "../controllers/hive.controller";
+import protect from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/gethive/:name", getHive);
-router.post("/createhive", createHive);
-router.post("/updatehive/:name", updateHive);
-router.delete("/deletehive/:name", deleteHive);
-router.post("/addhivemember/:name", addHiveMember);
-router.post("/removehivemember/:name", removeHiveMember);
+router.post("/createhive", protect, createHive);
+router.post("/updatehive/:name", protect, updateHive);
+router.delete("/deletehive/:name", protect, deleteHive);
+router.post("/addhivemember/:name", protect, addHiveMember);
+router.post("/removehivemember/:name", protect, removeHiveMember);
 
 export default router;
