9b8914a3c8153f527f75d5d1012c4d89748b60d7
Maghish <mail4mithran@gmail.com>
2024-07-13 13:01:07 +0530

    Added the 'followUser' controller function to the route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
-import { getUser, getCurrentUser } from "../controllers/user.controller";
+import {
+  getUser,
+  getCurrentUser,
+  followUser,
+} from "../controllers/user.controller";
 import protect from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.get("/getuser/:username", protect, getUser);
 router.post("/edituser", protect);
-router.post("/followuser/:username", protect);
+router.post("/followuser/:username", protect, followUser);
 router.post("/unfollowuser", protect);
 router.get("/getcurrentuser", protect, getCurrentUser);
 
