ffa6f9627eebb407e5bb79a9cf9f93c6083f4b48
Maghish <mail4mithran@gmail.com>
2024-07-17 20:36:01 +0530

    Created route for unfollowing user

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -3,6 +3,7 @@ import {
   getUser,
   getCurrentUser,
   followUser,
+  unfollowUser,
 } from "../controllers/user.controller";
 import protect from "../middleware/auth.middleware";
 
@@ -11,7 +12,7 @@ const router = Router();
 router.get("/getuser/:username", protect, getUser);
 router.post("/edituser", protect);
 router.post("/followuser/:username", protect, followUser);
-router.post("/unfollowuser", protect);
+router.post("/unfollowuser/:username", protect, unfollowUser);
 router.get("/getcurrentuser", protect, getCurrentUser);
 
 export default router;
