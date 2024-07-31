6fe6c887777ed4fa46edc5ed29113a17d53d25e9
Maghish <mail4mithran@gmail.com>
2024-07-31 21:23:13 +0530

    Modified Sidebar component

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,5 +1,7 @@
 function Sidebar() {
-  return <div className="h-full w-60 bg-white opacity-5"></div>;
+  return (
+    <div className="fixed top-0 left-0 h-full bg-[#0c0c0c] w-60 z-30"></div>
+  );
 }
 
 export default Sidebar;
