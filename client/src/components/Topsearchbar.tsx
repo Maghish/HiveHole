1fa96364530d24d049c206adfac2f045d56ff585
Maghish <mail4mithran@gmail.com>
2024-07-31 21:21:56 +0530

    Created new component: Topsearchbar

---
+++ client/src/components/Topsearchbar.tsx
@@ -0,0 +1,10 @@
+function Topsearchbar() {
+  return (
+    <div className="flex bg-[#1a1a1a] m-auto w-[400px] h-full mx-auto rounded-lg">
+      <div className="bg-inherit w-16 h-full rounded-tl-lg rounded-bl-lg"></div>
+      <input type="text" className="w-full h-full" />
+    </div>
+  );
+}
+
+export default Topsearchbar;
