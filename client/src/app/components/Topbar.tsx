be8dffb584c0aa49be5966a2bf9ea8b3757780e7
Maghish <mail4mithran@gmail.com>
2024-08-01 21:19:00 +0530

    Completed the Top Navbar (except the user butotn)

--- client/src/app/components/Topbar.tsx
+++ client/src/app/components/Topbar.tsx
@@ -2,28 +2,38 @@ import Topsearchbar from "./Topsearchbar";
 import { FaBell } from "react-icons/fa6";
 import { FaStar } from "react-icons/fa6";
 import { FaUserCircle } from "react-icons/fa";
+
 import Bell from "@/app/assets/Bell.svg";
 import OutlineBell from "@/app/assets/OutlineBell.svg";
 import Star from "@/app/assets/Star.svg";
+import OutlineStar from "@/app/assets/OutlineStar.svg";
+
 import Image from "next/image";
 import { useState } from "react";
 
 function Topbar() {
   const [bellFocused, setBellFocused] = useState(false);
+  const [favoriteFocused, setFavoriteFocused] = useState(false);
 
   return (
     <div className="flex fixed top-0 left-0 w-full h-[60px] px-10 py-3 z-10 bg-black">
       <Topsearchbar />
-      <div
-        className="flex w-auto h-full justify-stretch gap-x-5"
-        onFocus={() => setBellFocused(true)}
-        onBlur={() => setBellFocused(false)}
-      >
-        <button className="cursor-pointer self-center">
+      <div className="flex w-auto h-full justify-stretch gap-x-5">
+        <button
+          className="cursor-pointer self-center"
+          onFocus={() => setBellFocused(true)}
+          onBlur={() => setBellFocused(false)}
+        >
           <Image alt="notification" src={bellFocused ? OutlineBell : Bell} />
         </button>
 
-        <Image alt="favorite" src={Star} />
+        <button
+          className="cursor-pointer self-center"
+          onFocus={() => setFavoriteFocused(true)}
+          onBlur={() => setFavoriteFocused(false)}
+        >
+          <Image alt="favorite" src={favoriteFocused ? OutlineStar : Star} />
+        </button>
 
         {/* <button className="self-center flex items-center cursor-pointer focus:animate-pulse">
           <FaBell color="#D9D9D9" size="22px" />
