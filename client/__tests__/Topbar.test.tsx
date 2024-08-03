5ac66cecc566621e69e815c651ddf9943836e15d
Maghish <mail4mithran@gmail.com>
2024-08-03 21:43:53 +0530

    Sample template test case

---
+++ client/__tests__/Topbar.test.tsx
@@ -0,0 +1,9 @@
+// import { render, screen } from "@testing-library/react";
+
+// describe("", () => { // <-- Testsuite name
+//   it("", () => { // <-- Testcase name
+//     render(); // <-- Render component here
+
+//     expect(screen.getByTestId("")).toBeInTheDocument(); // <-- `data-testid` attribute of the element
+//   });
+// });
