09a38923cea8dca121685469e0c4ea973e7b5c4d
Maghish <mail4mithran@gmail.com>
2024-08-04 20:35:52 +0530

    Title for the sidebar

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -1,6 +1,15 @@
 function Sidebar() {
   return (
-    <div className="fixed top-0 left-0 h-full bg-[#0c0c0c] w-60 z-30"></div>
+    <div className="fixed top-0 left-0 h-full bg-[#0c0c0c] w-60 z-30 pt-8 px-4">
+      <div className="flex-col justify-center items-center">
+        <h2 className="w-full text-center text-2xl font-jetbrains-mono-bold text-white">
+          Hive Hole
+        </h2>
+        <p className="w-full text-center text-white text-opacity-70 font-jetbrains-mono-regular text-xs">
+          Inspired By Reddit
+        </p>
+      </div>
+    </div>
   );
 }
 
