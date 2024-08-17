a020226a3741295ba99f3d71c6cfcc89e415e61d
Maghish <mail4mithran@gmail.com>
2024-08-17 12:16:33 +0530

    Created new buttons: Home, Popular

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -1,14 +1,34 @@
+import { MdOutlineStackedLineChart, MdHome } from "react-icons/md";
+
 function Sidebar() {
   return (
-    <div className="fixed top-0 left-0 h-full bg-[#0c0c0c] w-60 z-30 pt-8 px-4">
-      <div className="flex-col justify-center items-center">
-        <h2 className="w-full text-center text-2xl font-jetbrains-mono-bold text-white">
-          Hive Hole
-        </h2>
-        <p className="w-full text-center text-white text-opacity-70 font-jetbrains-mono-regular text-xs">
-          Inspired By Reddit
-        </p>
+    <div className="fixed top-0 left-0 h-full bg-[#0c0c0c] w-60 z-30 pt-8 px-4 flex-col justify-center items-center">
+      {/* <div className="flex-col justify-center items-center"> */}
+      <h2 className="w-full text-center text-2xl font-jetbrains-mono-bold text-white">
+        Hive Hole
+      </h2>
+      <p className="w-full text-center text-white text-opacity-70 font-jetbrains-mono-regular text-xs">
+        Inspired By Reddit
+      </p>
+      <div className="flex-col gap-y-6 w-full h-auto mt-8">
+        <button className="cursor-pointer flex flex-row w-full px-6 py-2 rounded-full bg-transparent hover:bg-stone-800 transition-colors duration-75 ease-linear">
+          <MdHome color="#FFFFFF" size="18px" className="self-center" />
+          <p className="font-jetbrains-mono-regular text-base ml-2 text-white">
+            Home
+          </p>
+        </button>
+        <button className="cursor-pointer flex flex-row w-full px-6 py-2 rounded-full bg-transparent hover:bg-stone-800 transition-colors duration-75 ease-linear">
+          <MdOutlineStackedLineChart
+            color="#FFFFFF"
+            size="18px"
+            className="self-center"
+          />
+          <p className="font-jetbrains-mono-regular text-base ml-2 text-white">
+            Popular
+          </p>
+        </button>
       </div>
+      {/* </div> */}
     </div>
   );
 }
