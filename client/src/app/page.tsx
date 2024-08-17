abc46d022a43d9bc0280b3ba6edf12972b3c01dc
Maghish <mail4mithran@gmail.com>
2024-08-17 14:06:26 +0530

    Utilized AuthContext

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,38 @@
 "use client";
 
+import { useContext, useEffect } from "react";
 import axios from "axios";
 import Sidebar from "@/app/components/Sidebar";
 import Topbar from "@/app/components/Topbar";
 
+import { AuthContext } from "./contexts/AuthContext";
+import { redirect } from "next/navigation";
+
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVERURL;
 
 function Home() {
+  const { mode, userData } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (mode !== "User") {
+      redirect("/auth/login");
+    }
+  }, []);
+
   return (
     <div className="bg-black min-w-full h-screen">
-      <Sidebar />
-      <Topbar />
+      {mode === "User" ? (
+        <>
+          <Sidebar />
+          <Topbar />
+        </>
+      ) : (
+        <div className="absolute min-w-full h-screen flex items-center justify-center">
+          <p className="text-center text-xl font-jetbrains-mono-bold text-white">
+            You need to logged in!
+          </p>
+        </div>
+      )}
     </div>
   );
 }
