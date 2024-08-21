d9eae8dc956c6929501e53c8cc69f6eb61bcf8e7
Maghish <mail4mithran@gmail.com>
2024-08-21 23:10:05 +0530

    DID SOMETHING

--- client/__tests__/Topbar.test.tsx
+++
@@ -1,9 +0,0 @@
-// import { render, screen } from "@testing-library/react";
-
-// describe("", () => { // <-- Testsuite name
-//   it("", () => { // <-- Testcase name
-//     render(); // <-- Render component here
-
-//     expect(screen.getByTestId("")).toBeInTheDocument(); // <-- `data-testid` attribute of the element
-//   });
-// });
