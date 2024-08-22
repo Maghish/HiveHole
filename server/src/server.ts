6e8c9816dd439af8cfe76db027048ae3dc119eae
Maghish <mail4mithran@gmail.com>
2024-08-22 20:45:34 +0530

    Reverted RAGE integration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { App } from "@rage-js/core";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 import hiveRoute from "./routes/hive.route";
@@ -21,23 +21,9 @@ app.use("/api/hive", hiveRoute);
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
-// mongoose.connect(process.env.MONGOURI!).then(() => {
-//   console.log("Connected to MongoDB");
-//   app.listen(process.env.PORT!, () => {
-//     console.log("Listening on port " + process.env.PORT);
-//   });
-// });
-
-const rageApp = new App("./rage.config.json", true);
-
-async function start() {
-  await rageApp.setup();
-  await rageApp.start();
-}
-
-start();
-
-process.on("SIGINT" || "SIGQUIT", async () => {
-  await rageApp.stop();
-  process.exit(0);
+mongoose.connect(process.env.MONGOURI!).then(() => {
+  console.log("Connected to MongoDB");
+  app.listen(process.env.PORT!, () => {
+    console.log("Listening on port " + process.env.PORT);
+  });
 });
