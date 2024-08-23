4eafdd21725de87ec782212a8e7692f508b4d05d
Maghish <mail4mithran@gmail.com>
2024-08-23 22:03:46 +0530

    Created signup modal

---
+++ client/src/app/components/authForms/SignupForm.tsx
@@ -0,0 +1,9 @@
+function SignupForn() {
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <div className="max-w-[400px] w-[400px] max-h-[450px] h-[450px] bg-SecondaryBackgroundColor rounded-2xl shadow-FormModal"></div>
+    </div>
+  );
+}
+
+export default SignupForn;
