ad4efb8a114ec6ac5f48ab623fc13d74b1f36a60
Maghish <mail4mithran@gmail.com>
2024-08-29 22:24:48 +0530

    Added ErrorBox on loginform

--- client/src/app/components/authForms/LoginForm.tsx
+++ client/src/app/components/authForms/LoginForm.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import LoginFunc from "../LoginFunc";
 import SetCookie from "@/app/util/SetCookie";
+import FormErrorBox from "../FormErrorBox";
 
 interface LoginFormProps {
   setLoginFormVisibility: (v: boolean) => void;
@@ -15,11 +16,33 @@ function LoginForm({
   const [username, setUsername] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [errorBox, setErrorBox] = useState<false | string>("");
+
+  async function errorChecks() {
+    if (username == "" || email == "" || password == "") {
+      setErrorBox("Please fill up all fields");
+      return false;
+    }
+
+    if (username.length < 6) {
+      setErrorBox("Username must be at least 6 characters long");
+      return false;
+    }
+
+    if (email.length < 5) {
+      setErrorBox("Email must be at least 5 characters long");
+      return false;
+    }
+
+    return true;
+  }
 
   async function LoginUser() {
-    const response = await LoginFunc({ username, email, password });
+    if (!(await errorChecks())) {
+      return;
+    }
 
-    console.log(response);
+    const response = await LoginFunc({ username, email, password });
 
     // If response has properties like token, it means that it is not undefined and it's well-fetched
     if (response.token) {
@@ -27,12 +50,17 @@ function LoginForm({
 
       setLoginFormVisibility(false);
       window.location.reload();
+
+      return;
+    } else {
+      setErrorBox(response.message);
+      return;
     }
   }
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
-      <form className="relative max-w-[400px] w-[400px] max-h-[525px] h-[525px] bg-SecondaryBackgroundColor rounded-2xl shadow-FormModal flex flex-col px-12 py-10">
+      <form className="relative max-w-[400px] w-[400px] max-h-[560px] h-[560px] bg-SecondaryBackgroundColor rounded-2xl shadow-FormModal flex flex-col px-12 py-10">
         <IoMdClose
           className="absolute top-4 right-4 cursor-pointer transition-all ease-out duration-100 hover:bg-white hover:bg-opacity-15 rounded-full p-1.5"
           onClick={() => {
@@ -44,6 +72,7 @@ function LoginForm({
         <h3 className="text-white font-jetbrains-mono-regular text-center text-lg">
           Login
         </h3>
+        {errorBox != false ? <FormErrorBox error={errorBox} /> : ""}
         <div className=" mt-3 flex flex-col gap-y-4">
           <div className="flex flex-col gap-y-2">
             <label className="text-ModalSecondaryTextColor font-jetbrains-mono-regular text-sm ml-1">
