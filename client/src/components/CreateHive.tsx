ef4f5437448a4ef30c4e667d853c4b60c6c751ea
Maghish <mail4mithran@gmail.com>
2024-09-03 23:27:47 +0530

    Tags Input Field - CreateHive - Completely finished (I think)

--- client/src/components/CreateHive.tsx
+++ client/src/components/CreateHive.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import FormErrorBox from "./FormErrorBox";
 
@@ -9,10 +9,15 @@ interface CreateHiveProps {
 }
 
 function CreateHive({ setCreateHiveFormVisibility }: CreateHiveProps) {
+  const tagsInputRef = useRef(null);
+
   const [displayName, setDisplayName] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
+
   const [tags, setTags] = useState<string[]>([]);
+  const [currentTag, setCurrentTag] = useState<string>("");
+
   const [errorBox, setErrorBox] = useState<false | string>(false);
 
   return (
@@ -77,7 +82,7 @@ function CreateHive({ setCreateHiveFormVisibility }: CreateHiveProps) {
             <label className="text-ModalSecondaryTextColor font-jetbrains-mono-regular text-sm ml-1">
               Tags <span className="text-xs">(Maximum 3 tags)</span>
             </label>
-            <div className="flex flex-row gap-x-4 items-center w-full overflow-x-clip rounded-lg h-14 font-jetbrains-mono-regular px-4 py-2 outline-none text-xs sm:text-sm bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor">
+            <div className="relative flex flex-row gap-x-4 items-center w-full rounded-lg h-14 font-jetbrains-mono-regular px-4 py-2 outline-none text-xs sm:text-sm bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor">
               {/* <div className="inline-flex items-center gap-x-2 w-max h-max pl-4 pr-2 py-1 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
                 Tag 1
                 <IoMdClose
@@ -86,15 +91,46 @@ function CreateHive({ setCreateHiveFormVisibility }: CreateHiveProps) {
                   color="#FFFFFF"
                 />
               </div> */}
-              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
-                Tag 2
-              </div>
-              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
-                Tag 3
-              </div>
-              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
-                Tag 2
-              </div>
+
+              {tags.length > 0
+                ? tags.map((value, index) => (
+                    <div
+                      key={index}
+                      className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer"
+                    >
+                      {value}
+                    </div>
+                  ))
+                : ""}
+              {tags.length < 3 ? (
+                <input
+                  ref={tagsInputRef}
+                  className="relative bg-transparent text-sm text-ModalPrimaryTextColor w-full outline-none"
+                  onChange={(e) => {
+                    let inputFieldContent = e.target.value;
+                    if (inputFieldContent.length > 12) {
+                      inputFieldContent = inputFieldContent.slice(0, 10);
+                    }
+                    setCurrentTag(inputFieldContent);
+                  }}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      let inputFieldContent: string =
+                        // @ts-ignore
+                        tagsInputRef.current.value;
+
+                      if (inputFieldContent.length > 0) {
+                        setTags([...tags, currentTag]);
+                        // @ts-ignore
+                        tagsInputRef.current.value = "";
+                      }
+                    }
+                  }}
+                  placeholder="Add tag"
+                />
+              ) : (
+                ""
+              )}
             </div>
           </div>
         </div>
