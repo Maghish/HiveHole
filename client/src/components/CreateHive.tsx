09c5c03359ecf5884e987bec9f124717393ec513
Maghish <mail4mithran@gmail.com>
2024-09-03 22:53:47 +0530

    Tags Input Field - CreateHive Frontend placeholder complete

--- client/src/components/CreateHive.tsx
+++ client/src/components/CreateHive.tsx
@@ -75,9 +75,27 @@ function CreateHive({ setCreateHiveFormVisibility }: CreateHiveProps) {
           </div>
           <div className="flex flex-col gap-y-2">
             <label className="text-ModalSecondaryTextColor font-jetbrains-mono-regular text-sm ml-1">
-              Tags
+              Tags <span className="text-xs">(Maximum 3 tags)</span>
             </label>
-            <input />
+            <div className="flex flex-row gap-x-4 items-center w-full overflow-x-clip rounded-lg h-14 font-jetbrains-mono-regular px-4 py-2 outline-none text-xs sm:text-sm bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor">
+              {/* <div className="inline-flex items-center gap-x-2 w-max h-max pl-4 pr-2 py-1 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
+                Tag 1
+                <IoMdClose
+                  className="cursor-pointer transition-all ease-out duration-100 hover:bg-white hover:bg-opacity-15 rounded-full p-1.5"
+                  size="24px"
+                  color="#FFFFFF"
+                />
+              </div> */}
+              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
+                Tag 2
+              </div>
+              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
+                Tag 3
+              </div>
+              <div className="flex-shrink-0 w-max h-max px-4 py-1.5 rounded-lg font-jetbrains-mono-regular text-xs bg-ModalBackgroundColor text-ModalPrimaryTextColor ring-2 ring-ModalRingColor cursor-pointer">
+                Tag 2
+              </div>
+            </div>
           </div>
         </div>
       </form>
