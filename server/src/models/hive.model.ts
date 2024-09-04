8f67c640c51b854d0aa769cc97238030bcb60a31
Maghish <mail4mithran@gmail.com>
2024-09-04 10:51:58 +0530

    Edited hive model and capped few field's max chars limit

--- server/src/models/hive.model.ts
+++ server/src/models/hive.model.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 const hiveSchema = new mongoose.Schema(
   {
-    displayName: { type: String, required: true, maxLength: 50 },
-    name: { type: String, required: true, maxLength: 30 },
-    description: { type: String, required: true, maxLength: 300 },
+    displayName: { type: String, required: true, maxLength: 30 },
+    name: { type: String, required: true, maxLength: 20 },
+    description: { type: String, required: true, maxLength: 200 },
     // The user's username
     owner: { type: String, required: true },
     moderators: [{ type: String }],
